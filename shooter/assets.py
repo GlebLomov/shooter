04030af22a78b4777c1c72a716de897f2dec2f10
GlebLomov <lomgleb2019@gmail.com>
2025-04-06 13:28:03 +0300

    Add files via upload

---
+++ shooter/assets.py
@@ -0,0 +1,7 @@
+GAME_BG_IMG = "assets/sclad.jpg"
+ROCKET_IMG = "assets/nasa.png"
+ENEMY_IMG = "assets/vorog.png"
+GAME_MUSIC = "assets/bg_music.mp3"
+FIRE_SOUND = "assets/vogon.mp3"
+DAMAGE_SOUND = "assets/uron.mp3"
+BULLET_IMG = "assets/pulya.png"
---
+++ shooter/game.py
@@ -0,0 +1,175 @@
+from pygame import *
+from assets import *
+from random import randint
+
+# фонова музика
+mixer.init()
+mixer.music.load(GAME_MUSIC)
+mixer.music.play(-1)
+fire_sound = mixer.Sound(FIRE_SOUND)
+damage_sound = mixer.Sound(DAMAGE_SOUND)
+
+# нам потрібні такі картинки:
+img_back = GAME_BG_IMG  # фон гри
+img_hero = ROCKET_IMG  # герой
+img_enemy = ENEMY_IMG # ворог
+img_bullet = BULLET_IMG # das war ein befehl die 
+
+font.init()
+font2 = font.Font(None,36)
+font1 = font.Font(None,80)
+win = font1.render('YOU WIN!', True, (255, 255, 255))
+lose = font1.render('YOU LOSE!', True, (180,0,0))
+score = 0
+lost = 0
+max_lost = 3
+goal = 3
+# клас-батько для інших спрайтів
+class GameSprite(sprite.Sprite):
+    # конструктор класу
+    def __init__(self, player_image, player_x, player_y, 
+                 size_x, size_y, player_speed):
+        # викликаємо конструктор класу (Sprite):
+        sprite.Sprite.__init__(self)
+        # кожен спрайт повинен зберігати властивість image - зображення
+        self.image = transform.scale(
+            image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        # кожен спрайт повинен зберігати властивість rect - прямокутник, в який він вписаний
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    # метод, що малює героя на вікні
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+# клас головного гравця
+class GameSprite(sprite.Sprite):
+    def __init__(self,player_image,player_x,player_y,size_x,size_y,player_speed):
+        sprite.Sprite.__init__(self)
+        self.image = transform.scale(image.load(player_image),(size_x,size_y))
+        self.speed = player_speed
+
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y 
+
+
+    def reset(self):
+        window.blit(self.image,(self.rect.x,self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x >5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+    def fire(self):
+        bullet = Bullet(img_bullet,self.rect.centerx,self.rect.top,60,80,-15)
+        bullets.add(bullet) 
+        
+
+class Enemy(GameSprite):
+    def update(self):
+        self.rect.x -= self.speed
+        global lost
+        if self.rect.y >win_width:
+            self.rect.x = randint(80,win_width -80)
+            self.rect.y = 0 
+            lost = lost - 1
+
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y < 0:
+            self.kill()
+
+
+
+
+win_width = 700
+win_height = 500
+display.set_caption("Shooter")
+window = display.set_mode((win_width,win_height))
+background = transform.scale(image.load(img_back),(win_width,win_height))
+
+ship = Player(img_hero,5,win_height - 100,80,100,10)
+monsters = sprite.Group()
+for i in range (1,6):
+    monster = Enemy(img_enemy,randint(200,700), randint(-15,700),80,50,randint(1,5))
+    monsters.add(monster)
+bullets = sprite.Group()
+
+start_time = time.get_ticks()
+shots_fired = 0
+
+def restart_game():
+    global score,lost,finish,monster,bullets,ship,start_time,shots_fired
+    score = 0
+    lost = 0
+    shots_fired = 0
+    finish = False
+    ship = Player(img_hero,5,win_height - 100,80,100,10)
+    monsters = sprite.Group()
+    for i in range (1,6):
+        monster = Enemy(img_enemy,randint(200,700), randint(-15,700),80,50,randint(1,5))
+        monsters.add(monster)
+    bullets = sprite.Group()
+
+finish = False
+run = True
+ # прапорець скидається кнопкою закриття вікна
+while run:
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE and not finish:
+                fire_sound.play()
+                ship.fire()
+                shots_fired +=1
+            elif e.key == K_RETURN and finish:
+                restart_game()
+    if not finish:
+        window.blit(background, (0, 0))
+        text = font2.render("Рахунок: " + str(score), 1, (255,255,255))
+        window.blit(text, (10,20))
+        text_lose = font2.render("Пропущено: " + str(lost), 1, (255,255,255))
+        window.blit(text_lose, (10,50))
+        # Обчислення таймера (у секундах)
+        elapsed_time = (time.get_ticks() - start_time) // 1000
+        timer_text = font2.render("Час: " + str(elapsed_time) + "с", 1, (255, 255, 255))
+        window.blit(timer_text, timer_text.get_rect(topright=(win_width - 10, 20)))
+        # Лічильник пострілів
+        shots_text = font2.render("Постріли: " + str(shots_fired), 1, (255, 255, 255))
+        window.blit(shots_text, shots_text.get_rect(topright=(win_width - 10, 50)))
+        ship.update()
+        monsters.update()
+        ship.reset()
+        monsters.draw(window)
+        bullets.update()
+        bullets.draw(window)
+        
+        collides = sprite.groupcollide(monsters,bullets,True,True)
+        for c in collides:  
+            score = score +1
+            monster = Enemy(img_enemy,randint(200,700), randint(-15,700),80,50,randint(1,5))
+            monsters.add(monster)
+
+        if sprite.spritecollide(ship,monsters,False) or lost >= max_lost:
+            finish = True
+            window.blit(lose,(200,200))
+            retry_text = font2.render("Натисніть Enter, щоб почати спочатку",True,(255,255,255))
+            window.blit(retry_text,(100,300))
+        if score >= goal:
+            finish = True
+            window.blit(win,(200,200))
+            retry_text = font2.render("Натисніть Enter, щоб почати спочатку",True,(255,255,255))
+            window.blit(retry_text,(100,300))        
+        display.update()
+    time.delay(50)
